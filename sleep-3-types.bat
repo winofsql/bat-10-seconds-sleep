8eef64b25b48d530cd98cb440182b20dc6c1aa97
sworc <lightbox@nifty.com>
2021-09-02 13:29:00 +0900

    Add files via upload

---
+++ sleep-3-types.bat
@@ -0,0 +1,11 @@
+REM Žg—p•û–@o—Í
+timeout /? > timeout.txt
+
+REM 10 •b’âŽ~
+timeout /T 10
+
+REM 10 •b’âŽ~
+powershell sleep -m 10000
+
+REM 10 •b’âŽ~
+echo wscript.sleep(10000) > sleep.vbs & cscript sleep.vbs
